a2db8624df11baffb5c02c6dc2fd3460539d820f
jipunroy95 <jipunroy95@gmail.com>
2026-08-19 22:04:24 +0600

    Cinema Ticket Counter

---
+++ dist/problem 1.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function getTicketPrice(age) {
+    if (age < 5) {
+        return 0;
+    }
+    else if (age <= 12) {
+        return 100;
+    }
+    else if (age <= 59) {
+        return 200;
+    }
+    else {
+        return 120;
+    }
+}
+//# sourceMappingURL=problem%201.js.map
---
+++ src/problem 1.ts
@@ -0,0 +1,12 @@
+function getTicketPrice(age: number): number {
+    if (age < 5) {
+        return 0;
+    } else if (age <= 12) {
+        return 100;
+    } else if (age <= 59) {
+        return 200;
+    } else {
+        return 120;
+    }
+}
+
